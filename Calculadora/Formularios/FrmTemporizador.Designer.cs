225da12af20b9829cc34f07a1bdb9aba14b45d36
Elisis-5002 <ana.elisa.bailon@gmail.com>
2026-02-09 15:48:00 -0600

    Se agrega una alarma

--- Calculadora/Formularios/FrmTemporizador.Designer.cs
+++ Calculadora/Formularios/FrmTemporizador.Designer.cs
@@ -31,6 +31,10 @@
             components = new System.ComponentModel.Container();
             labelHora = new Label();
             tmrHora = new System.Windows.Forms.Timer(components);
+            menuStrip1 = new MenuStrip();
+            establecerToolStripMenuItem = new ToolStripMenuItem();
+            mIalarma1 = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
             // 
             // labelHora
@@ -49,14 +53,42 @@
             tmrHora.Interval = 1000;
             tmrHora.Tick += tmrHora_Tick;
             // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(24, 24);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { establecerToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 33);
+            menuStrip1.TabIndex = 1;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // establecerToolStripMenuItem
+            // 
+            establecerToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { mIalarma1 });
+            establecerToolStripMenuItem.Name = "establecerToolStripMenuItem";
+            establecerToolStripMenuItem.Size = new Size(107, 29);
+            establecerToolStripMenuItem.Text = "Establecer";
+            // 
+            // mIalarma1
+            // 
+            mIalarma1.Name = "mIalarma1";
+            mIalarma1.Size = new Size(270, 34);
+            mIalarma1.Text = "Alarma 1";
+            mIalarma1.Click += mIalarma1_Click;
+            // 
             // FrmTemporizador
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
             Controls.Add(labelHora);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "FrmTemporizador";
             Text = "Reloj";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -65,5 +97,8 @@
 
         private Label labelHora;
         private System.Windows.Forms.Timer tmrHora;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem establecerToolStripMenuItem;
+        private ToolStripMenuItem mIalarma1;
     }
 }
